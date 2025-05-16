ea3773592ab3e7d211cf6487aff0f7adc30dc822
s3vro <sevro@gmx.de>
2025-05-16 11:30:55 +0200

    Constant Folding for Div and Mod, maybe works? maybe i forgor something

--- src/main/java/edu/kit/kastel/vads/compiler/ir/optimize/ConstantFolding.java
+++ src/main/java/edu/kit/kastel/vads/compiler/ir/optimize/ConstantFolding.java
@@ -6,16 +6,25 @@ public class ConstantFolding implements Optimizer {
     @Override
     public Node transform(Node node) {
         if (node instanceof BinaryOperationNode) {
-            if (node.predecessor(0) instanceof ConstIntNode && node.predecessor(1) instanceof ConstIntNode) {
+            if (node.predecessor(BinaryOperationNode.LEFT) instanceof ConstIntNode
+                    && node.predecessor(BinaryOperationNode.RIGHT) instanceof ConstIntNode) {
                 int cL = ((ConstIntNode) node.predecessor(BinaryOperationNode.LEFT)).value();
                 int cR = ((ConstIntNode) node.predecessor(BinaryOperationNode.RIGHT)).value();
-
-                System.out.println(cL + "FOLDED" + cR);
 
                 return switch(node) {
                     case AddNode _ -> new ConstIntNode(node.block(), cL + cR);
                     case SubNode _ -> new ConstIntNode(node.block(), cL - cR);
                     case MulNode _ -> new ConstIntNode(node.block(), cL * cR);
+                    case DivNode _ -> {
+                        if (cR == 0) yield node;
+                        if (cL == Integer.MIN_VALUE && cR == -1) yield node;
+                        yield new ConstIntNode(node.block(), cL / cR);
+                    }
+                    case ModNode _ -> {
+                        if (cR == 0) yield node;
+                        if (cL == Integer.MIN_VALUE && cR == -1) yield node;
+                        yield new ConstIntNode(node.block(), cL % cR);
+                    }
                     default -> node;
                 };
             }
